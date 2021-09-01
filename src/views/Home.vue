b671583e73820c2d99ec6e2c993f1f10ff5b3779
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-09-01 19:15:01 +0800

    implement select country component for each covid data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <main v-if="!loading">
     <data-title :text="title" :dataDate="dataDate"/>
     <data-boxes :stats="stats"/>
+    <country-select @getCountry="loadCountryData" :countries="countries"/>
+    <button @click="clearCountryData" v-if="stats.Country" class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600">Clear Country</button>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -14,6 +16,7 @@
 <script>
 import DataTitle from '@/components/DataTitle'
 import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'Home',
@@ -28,20 +31,31 @@ export default {
     }
   },
   components: {
-    DataTitle, DataBoxes
+    DataTitle, DataBoxes, CountrySelect
   },
   methods: {
     async fetchCovidData() {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
+    },
+    loadCountryData(country) {
+      this.stats = country
+      this.title = country.Country
+    },
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData();
+      this.title = "Global"
+      this.stats = data.Global
+      this.loading = false
     }
   },
   async created() {
     const data = await this.fetchCovidData();
     this.dataDate = data.Date
     this.stats = data.Global
-    this.countries = data.countries
+    this.countries = data.Countries
     this.loading = false
 
     console.log(data);
